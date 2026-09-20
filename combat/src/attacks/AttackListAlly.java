2e31b45277734a3735d82cc93ba972c456889e54
Ace10267 <harshitrajbansal2610@gmail.com>
2026-09-20 03:35:26 -0700

    reverted to old version
    rpg game

---
+++ combat/src/attacks/AttackListAlly.java
@@ -0,0 +1,13 @@
+package attacks;
+
+import java.util.ArrayList;
+
+public class AttackListAlly {
+    
+    private ArrayList<Attack> attacks = new ArrayList<Attack>();
+    Attack counterattack = new Attack(10, "neutral", 20);
+    Attack punch = new Attack(5, "neutral", 20);
+
+
+
+}
